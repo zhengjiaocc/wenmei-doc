3be5cf3474809f525849a503910f619d1ef4eaec
zhanghanxiao <mailblankpage@163.com>
2024-07-04 17:58:42 +0800

    fix

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   ['meta', { name: 'description', content: '《问题妹妹恋上我》文档库，负是非' }],
   ],
   sitemap: {
-    hostname: 'https://www.wtmmlsw.cn/'
+    hostname: 'https://www.wtmmlsw.cn'
   },
 
   appearance:false, // 默认配置，可以切换
