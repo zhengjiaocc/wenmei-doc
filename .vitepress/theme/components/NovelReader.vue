0f88eaed278c1aa47bb20af691dcf18325ebd39c
zhanghanxiao <mailblankpage@163.com>
2024-07-08 17:57:33 +0800

    fix

--- .vitepress/theme/components/NovelReader.vue
+++ .vitepress/theme/components/NovelReader.vue
@@ -263,7 +263,7 @@ export default {
   align-items: center;
   height: 45px;
   /* box-shadow: 0 -4px 8px rgba(0,0,0,0.1); */
-  border-top: 1px solid #e0e0e0;
+  /* border-top: 1px solid #e0e0e0; */
   z-index: 1000; /* 确保在其他元素之上 */
 }
 
