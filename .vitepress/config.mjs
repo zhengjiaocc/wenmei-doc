daed6907c105e7e7ede84880e319b0d2a7780708
zhanghanxiao <mailblankpage@163.com>
2024-07-09 03:24:18 +0800

    回滚

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,15 +7,6 @@ export default defineConfig({
   head: [
     ["link", { rel: "icon", href: "wtmmlsw.svg" }],
     ['meta', { name: 'description', content: '《问题妹妹恋上我》文档库，负是非' }],
-    // 引入 51.la 的统计脚本
-    ['script', { id: 'LA_COLLECT', charset: 'UTF-8', src: 'https//sdk.51.la/js-sdk-pro.min.js' }],
-    ['script', {},
-      `
-         window.onload = function() {
-           LA.init({id:"3Iyfpiu76v1utPQC",ck:"3Iyfpiu76v1utPQC"});
-         };
-         `
-    ],
   ],
   sitemap: {
     hostname: 'https://www.wtmmlsw.cn'
