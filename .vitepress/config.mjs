87cf7e5b7e05f83ee0149b45f88f7b746789e3a5
zhanghanxiao <mailblankpage@163.com>
2024-07-11 02:03:39 +0800

    fix

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,13 +4,6 @@ export default defineConfig({
   head: [
     ["link", { rel: "icon", href: "wtmmlsw.svg" }],
     ['meta', { name: 'description', content: '《问题妹妹恋上我》文档库，负是非' }],
-    [
-      'script',
-      {},
-      `
-     !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3Iyfpiu76v1utPQC",ck:"3Iyfpiu76v1utPQC"});
-      `,
-    ],
   ],
   sitemap: {
     hostname: 'https://www.wtmmlsw.cn'
