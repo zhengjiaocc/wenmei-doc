326d28516f03e19758201f662cc107325426eafc
zhanghanxiao <mailblankpage@163.com>
2024-07-23 08:38:12 +0800

    fix

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -16,7 +16,9 @@ import './style/index.css'
 
 DefaultTheme.enhanceApp = ({ app, router, siteData }) => {
   router.onBeforeRouteChange = (to) => {
+    window.TWIKOO_MAGIC_PATH =null;
     console.log('路由将改变为: ', to);
+
     if (typeof _hmt !== 'undefined') {
       _hmt.push(['_trackPageview', to]);
     }
