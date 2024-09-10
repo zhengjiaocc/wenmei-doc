34eed7aa9d7e7cbcb449947de45c6bb5befbe9ab
zhanghanxiao <mailblankpage@163.com>
2024-09-11 01:18:00 +0800

    配置域名访问

--- .vitepress/theme/components/Visit.vue
+++ .vitepress/theme/components/Visit.vue
@@ -70,7 +70,7 @@ export default {
     async fetchStatistics() {
       try {
         const response = await axios.get(
-          "https://152.32.210.227:10002/proxy/baidu/base"
+          "http://tj.zhengjiao.cc:10002/proxy/baidu/base"
         );
         const data = response.data;
         this.todayVisitors = data["今日访问人数"];
