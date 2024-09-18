b5228ef85ef60f5b9c3c2c07b86640bb0aa3afb8
zhanghanxiao <mailblankpage@163.com>
2024-09-19 00:57:22 +0800

    发布公告

--- .vitepress/theme/components/notice.vue
+++ .vitepress/theme/components/notice.vue
@@ -1,42 +1,80 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const visible = ref(true)
+import { ref, onMounted } from 'vue'
+
+// 当前公告的ID
+const currentBulletinId = '2024091901'; // 示例公告ID
+const visible = ref(true);
+const closeMessage = ref(false); // 控制是否显示关闭提示信息
+
+// 页面加载时执行
+onMounted(() => {
+  // 从本地存储获取上次公告ID
+  const storedBulletinId = localStorage.getItem('bulletinId');
+  
+  // 如果存储的公告ID与当前公告ID一致，则不显示公告
+//   if (storedBulletinId === currentBulletinId) {
+//     visible.value = false;
+//   }
+});
 
 function closeBulletin() {
-    visible.value = false
+  // 将当前公告ID存储到本地存储
+  localStorage.setItem('bulletinId', currentBulletinId);
+  
+  // 隐藏公告
+  visible.value = false;
+
+  // 显示关闭提示信息
+  closeMessage.value = true;
+
+  // 3秒后自动隐藏提示信息
+  setTimeout(() => {
+    closeMessage.value = false;
+  }, 6000);
 }
 </script>
 
 <template>
+  <div>
+    <!-- 公告弹窗 -->
     <div v-if="visible" class="bulletin-wrapper" style="width: 300px;">
+      <div class="bulletin-title">
+        <span class="bulletin-icon left">
+          <svg class="bulletin-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" style="width: 20px; height: 20px; font-size: 20px; color: rgb(255, 255, 255);">
+            <path d="M27.16 8.08l-1.53 1.29a10 10 0 0 1-.29 13.23l1.47 1.4a12 12 0 0 0 .35-15.88z" fill="currentColor"></path>
+            <path d="M21.58 12a6 6 0 0 1-.18 7.94l1.47 1.36a8 8 0 0 0 .23-10.59z" fill="currentColor"></path>
+            <path d="M18 30a1 1 0 0 1-.71-.3L9.67 22H3a1 1 0 0 1-1-1V11a1 1 0 0 1 1-1h6.67l7.62-7.7a1 1 0 0 1 1.41 0a1 1 0 0 1 .3.7v26a1 1 0 0 1-1 1zM4 20h6.08a1 1 0 0 1 .71.3L17 26.57V5.43l-6.21 6.27a1 1 0 0 1-.71.3H4z" fill="currentColor"></path>
+          </svg>
+          <span>公告</span>
+        </span>
+        <svg class="btn-close" @click="closeBulletin" t="1573745677073" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4448" width="22" height="22">
+          <path d="M512 34.133333a486.4 486.4 0 1 0 486.4 486.4A486.4 486.4 0 0 0 512 34.133333z m209.4848 632.8064l-55.6032 55.466667-151.517867-151.125333-151.517866 151.1168-55.6032-55.466667 151.517866-151.108267L307.242667 364.714667l55.6032-55.466667 151.517866 151.125333 151.517867-151.1168 55.6032 55.466667-151.517867 151.099733z m0 0" p-id="4449"></path>
+        </svg>
+      </div>
 
-        <div class="bulletin-title">
-            <span class="bulletin-icon left">
-                <svg class="bulletin-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" style="width: 20px; height: 20px; font-size: 20px; color: rgb(255, 255, 255);"><path d="M27.16 8.08l-1.53 1.29a10 10 0 0 1-.29 13.23l1.47 1.4a12 12 0 0 0 .35-15.88z" fill="currentColor"></path><path d="M21.58 12a6 6 0 0 1-.18 7.94l1.47 1.36a8 8 0 0 0 .23-10.59z" fill="currentColor"></path><path d="M18 30a1 1 0 0 1-.71-.3L9.67 22H3a1 1 0 0 1-1-1V11a1 1 0 0 1 1-1h6.67l7.62-7.7a1 1 0 0 1 1.41 0a1 1 0 0 1 .3.7v26a1 1 0 0 1-1 1zM4 20h6.08a1 1 0 0 1 .71.3L17 26.57V5.43l-6.21 6.27a1 1 0 0 1-.71.3H4z" fill="currentColor"></path></svg>
-                <span>公告</span>
-            </span>
-            <svg class="btn-close" @click="closeBulletin" t="1573745677073" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4448" width="22" height="22"><path d="M512 34.133333a486.4 486.4 0 1 0 486.4 486.4A486.4 486.4 0 0 0 512 34.133333z m209.4848 632.8064l-55.6032 55.466667-151.517867-151.125333-151.517866 151.1168-55.6032-55.466667 151.517866-151.108267L307.242667 364.714667l55.6032-55.466667 151.517866 151.125333 151.517867-151.1168 55.6032 55.466667-151.517867 151.099733z m0 0" p-id="4449"></path></svg>
-        </div>
+      <div class="bulletin-content">
+        <h3 class="bulletin-h3">本次更新：</h3>
 
-        <div class="bulletin-content">
-            <h3 class="bulletin-h3">本次更新：</h3>
-            <!-- <p class="bulletin-p">公告样式，参考自 <a class="bulletin-link" href="https://theme-reco.vuejs.press/" target="_blank">VuePress-Reco</a></p>
-            <p class="bulletin-p">QQ 频道：******(无效二维码)</p>
-            <img class="bulletin-img" src=""> -->
-            <p>版权归属[缘盟]</p>
+        <p style="color: red;">章节1900至1910已经更新</p>
+        <hr>
+        <p>粉丝加群,优先选择</p>
+        <p>2群-->261599177</p>
+        <p>4群--> 546542165</p>
+        
 
-            <p style="color: red;">章节1900至1910已经更新</p>
-            <hr>
-            <div class="btn-group">
-                <a class="btn" href="/docs/resources/resources" target="_blank">前往</a>
-            </div>
-
-
-
-        </div>
+        <!-- <hr>
+        <div class="btn-group">
+          <a class="btn" href="/docs/resources/resources" target="_blank">前往</a>
+        </div> -->
+      </div>
     </div>
-</template>
 
+    <!-- 关闭提示信息 -->
+    <div v-if="closeMessage" class="close-message">
+      本次关闭后将会在公告更新后弹出。
+    </div>
+  </div>
+</template>
 
 <style>
 .bulletin-wrapper {
@@ -50,8 +88,6 @@ function closeBulletin() {
     background-color: #1b1446;
     box-shadow: 0 10px 30px 0 rgb(0 0 0 / 40%);
 }
-
-
 .bulletin-title {
     display: flex;
     align-items: center;
@@ -59,34 +95,24 @@ function closeBulletin() {
     color: rgb(255 255 255);
     padding: 1rem;
 }
-
-
 .bulletin-icon {
     display: inline-flex;
 }
-
 .bulletin-icon.left {
     flex-direction: row;
-    align-items: center
+    align-items: center;
 }
-
 .bulletin-icon.left>svg {
-    margin-right: .375rem
+    margin-right: .375rem;
 }
-
-
 .btn-close {
     cursor: pointer;
     fill: currentColor;
 }
-
-
 .bulletin-content {
     padding: 1.5rem 2rem;
     background-color: #ffffff;
 }
-
-
 .bulletin-h3 {
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
@@ -94,25 +120,18 @@ function closeBulletin() {
     font-size: 19px;
     font-weight: bolder;
 }
-
 .bulletin-p {
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
     color: #000000;
     font-size: 16px;
 }
-
-
 .bulletin-link {
     color: red;
 }
-
-
 .bulletin-img {
-    width: 100%
+    width: 100%;
 }
-
-
 hr {
     margin-top: 1.5rem;
     margin-bottom: 1.5rem;
@@ -120,12 +139,9 @@ hr {
     border-style: solid;
     border-color: #1b1446;
 }
-
 .btn-group {
-    text-align: center
+    text-align: center;
 }
-
-
 .btn {
     display: inline-block;
     height: 3.5rem;
@@ -136,5 +152,26 @@ hr {
     text-align: center;
     line-height: 3.5rem;
     color: #ffffff;
+}
+/* 关闭提示信息样式 */
+.close-message {
+    position: fixed;
+    bottom: 2rem;
+    right: 2rem;
+    padding: 1rem 1.5rem;
+    background-color: #1b1446;
+    color: white;
+    border-radius: 0.5rem;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+    z-index: 40;
+    animation: fade-in-out 3s ease forwards;
+}
+
+/* 添加淡入淡出动画 */
+@keyframes fade-in-out {
+    0% { opacity: 0; transform: translateY(100%); }
+    10% { opacity: 1; transform: translateY(0); }
+    90% { opacity: 1; transform: translateY(0); }
+    100% { opacity: 0; transform: translateY(100%); }
 }
 </style>
